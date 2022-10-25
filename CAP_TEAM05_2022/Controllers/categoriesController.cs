5cf10d7c85d9b92b19bce0015189f11f130afbd7
QuocNam <tranquocnam201@gmail.com>
2022-10-25 23:10:50 +0700

    Add project files.

---
+++ CAP_TEAM05_2022/Controllers/categoriesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CAP_TEAM05_2022.Models;
+
+namespace CAP_TEAM05_2022.Controllers
+{
+    public class categoriesController : Controller
+    {
+        private QuyThanhEntities db = new QuyThanhEntities();
+
+        // GET: categories
+        public ActionResult Index()
+        {
+            var categories = db.categories.Include(c => c.user);
+            return View(categories.ToList());
+        }
+
+        // GET: categories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            category categories = db.categories.Find(id);
+            if (categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categories);
+        }
+
+        // GET: categories/Create
+        public ActionResult Create()
+        {
+            ViewBag.created_by = new SelectList(db.users, "id", "name");
+            return View();
+        }
+
+        // POST: categories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,code,created_by,name,slug,status,created_at,updated_at,deleted_at")] category categories)
+        {
+            if (ModelState.IsValid)
+            {
+                db.categories.Add(categories);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.created_by = new SelectList(db.users, "id", "name", categories.created_by);
+            return View(categories);
+        }
+
+        // GET: categories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            category categories = db.categories.Find(id);
+            if (categories == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.created_by = new SelectList(db.users, "id", "name", categories.created_by);
+            return View(categories);
+        }
+
+        // POST: categories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,code,created_by,name,slug,status,created_at,updated_at,deleted_at")] category categories)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(categories).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.created_by = new SelectList(db.users, "id", "name", categories.created_by);
+            return View(categories);
+        }
+
+        // GET: categories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            category categories = db.categories.Find(id);
+            if (categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categories);
+        }
+
+        // POST: categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            category categories = db.categories.Find(id);
+            db.categories.Remove(categories);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CAP_TEAM05_2022/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CAP_TEAM05_2022.Startup))]
+namespace CAP_TEAM05_2022
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
