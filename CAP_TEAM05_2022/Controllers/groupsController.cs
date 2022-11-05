91cc270d6e4f615735220cd672db75f4c4f2b7ae
QuocNam <tranquocnam201@gmail.com>
2022-11-05 21:39:45 +0700

    code module Group Product

---
+++ CAP_TEAM05_2022/Controllers/groupsController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CAP_TEAM05_2022.Helper;
+using CAP_TEAM05_2022.Models;
+
+namespace CAP_TEAM05_2022.Controllers
+{
+    public class groupsController : Controller
+    {
+        private QuyThanhEntities db = new QuyThanhEntities();
+
+        // GET: groups
+        public ActionResult Index()
+        {
+            var groups = db.groups.Include(g => g.user).Where(c => c.status != 3).OrderByDescending(c => c.id);
+            return View(groups.ToList());
+        }
+        public ActionResult Create_GroupProduct(string name_GroupProduct)
+        {
+            string email = Session["user_email"].ToString();
+            user user = db.users.Where(u => u.email == email).FirstOrDefault();
+            group GroupProduct = new group();
+            GroupProduct.name = name_GroupProduct;
+            GroupProduct.created_by = user.id;
+            GroupProduct.created_at = DateTime.Now;
+            GroupProduct.slug = name_GroupProduct;
+            GroupProduct.code = "NH" + CodeRandom.RandomCode();
+            db.groups.Add(GroupProduct);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete_GroupProduct(group GroupProducts)
+        {
+            group group = db.groups.Find(GroupProducts.id);
+            group.status = 3;
+            group.deleted_at = DateTime.Now;
+            db.Entry(group).State = EntityState.Modified;
+            db.SaveChanges();
+            return Json("Delete_GroupProduct", JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult FindGroupProduct(int GroupProduct_id)
+        {
+            group group = db.groups.Find(GroupProduct_id);
+            var emp = new group();
+            emp.id = GroupProduct_id;
+            emp.name = group.name;
+            return Json(emp);
+        }
+        public ActionResult Edit_GroupProduct(int GroupProduct_id, string name_GroupProduct)
+        {
+            group group = db.groups.Find(GroupProduct_id);
+            group.name = name_GroupProduct;
+            group.status = 2;
+            group.updated_at = DateTime.Now;
+            db.Entry(group).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: groups/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            group group = db.groups.Find(id);
+            if (group == null)
+            {
+                return HttpNotFound();
+            }
+            return View(group);
+        }
+
+        // GET: groups/Create
+        public ActionResult Create()
+        {
+            ViewBag.created_by = new SelectList(db.users, "id", "name");
+            return View();
+        }
+
+        // POST: groups/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,code,created_by,name,slug,status,created_at,updated_at,deleted_at")] group group)
+        {
+            if (ModelState.IsValid)
+            {
+                db.groups.Add(group);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.created_by = new SelectList(db.users, "id", "name", group.created_by);
+            return View(group);
+        }
+
+        // GET: groups/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            group group = db.groups.Find(id);
+            if (group == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.created_by = new SelectList(db.users, "id", "name", group.created_by);
+            return View(group);
+        }
+
+        // POST: groups/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,code,created_by,name,slug,status,created_at,updated_at,deleted_at")] group group)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(group).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.created_by = new SelectList(db.users, "id", "name", group.created_by);
+            return View(group);
+        }
+
+        // GET: groups/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            group group = db.groups.Find(id);
+            if (group == null)
+            {
+                return HttpNotFound();
+            }
+            return View(group);
+        }
+
+        // POST: groups/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            group group = db.groups.Find(id);
+            db.groups.Remove(group);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
