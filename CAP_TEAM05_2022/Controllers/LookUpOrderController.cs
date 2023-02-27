9738cad1ebb7a633a8e2490b317146191b6ba4ef
QuocNam <tranquocnam201@gmail.com>
2023-02-27 14:24:22 +0700

    tra cứu lịch sử đơn hàng

---
+++ CAP_TEAM05_2022/Controllers/LookUpOrderController.cs
@@ -0,0 +1,34 @@
+﻿using CAP_TEAM05_2022.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CAP_TEAM05_2022.Controllers
+{
+    public class LookUpOrderController : Controller
+    {
+        private CP25Team05Entities db = new CP25Team05Entities();
+        // GET: LookUpOrder
+        public ActionResult Index()
+        {
+            ViewBag.CodeCustomer = false;
+            return View();
+        }
+        public ActionResult LookUpOrder(string code_customer)
+        {
+            if (!String.IsNullOrEmpty(code_customer))
+            {
+                var customer = db.customers.Where(c => c.code == code_customer);
+                if (customer.Count() > 0)
+                {
+                    var sales = db.sales.Where(s => s.customer.code == code_customer);
+                    return View(sales.OrderByDescending(c => c.id).ToList());
+                }
+            }          
+            ViewBag.CodeCustomer = true;
+            return View("Index");
+        }
+    }
+}
