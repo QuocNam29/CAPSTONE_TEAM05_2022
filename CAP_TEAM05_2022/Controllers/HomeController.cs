66f3a630864bf44e9fa98b70a32c98f7c9b4465e
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-03-23 14:30:10 +0700

    Update errror controller

--- CAP_TEAM05_2022/Controllers/HomeController.cs
+++ CAP_TEAM05_2022/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace CAP_TEAM05_2022.Controllers
 {
@@ -24,10 +20,6 @@ namespace CAP_TEAM05_2022.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
-            return View();
-        } 
-        public ActionResult Error404()
-        {
             return View();
         }
     }
